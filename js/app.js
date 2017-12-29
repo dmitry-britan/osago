df7c7e6233b309f0a8790fb8ce2e19f402bf076e
Dmitry Britan <dmitry.britan@gmail.com>
2017-12-29 15:58:59 +0200

    bug fix

--- js/app.js
+++ js/app.js
@@ -1090,11 +1090,12 @@ $(document).ready(function(){
 
 		$window.on("resize", function(){
 			if ($window.outerWidth() <= BREAKPOINT_XS && !$methodBtns.filter("[data-for='byUpload']").hasClass("b-finalize__btn_active")){
-			//  console.log("viewport <= 767px");
-			 $methodBtns.filter("[data-for='byUpload']").trigger("click");
-			} else {
+				//  console.log("viewport <= 767px");
+				 $methodBtns.filter("[data-for='byUpload']").trigger("click");
+				} 
+			if ($window.outerWidth() > BREAKPOINT_XS && $methodBtns.filter("[data-for='byUpload']").hasClass("b-finalize__btn_active")) {
 			//  console.log("viewport > 767px");
-			 $methodBtns.filter("[data-for='bySelf']").trigger("click");
+				$methodBtns.filter("[data-for='bySelf']").trigger("click");
 			}
 		 });
 
